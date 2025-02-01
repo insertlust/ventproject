8555f88f7e83c5ba2cbbc84b22c49c641090a1cf
insertlust <angelo.m.espejo@gmail.com>
2025-02-01 14:22:43 -0800

    Add files via upload

---
+++ static/sw.js
@@ -0,0 +1,15 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("v1").then((cache) => {
+      return cache.addAll(["/", "/static/style.css", "/static/background.jpg"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});

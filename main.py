05bc20ef58c973907dac9455c4843686bcd863c3
insertlust <angelo.m.espejo@gmail.com>
2025-02-02 01:44:16 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/results", methods=["POST"])
+def results():
+    address = request.form.get("address")
+    return render_template("results.html", address=address)
 
 @app.route("/about")
 def about():
@@ -15,4 +20,4 @@ def donations():
     return render_template("donations.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8080, debug=True)
